a7acdc4c14148b22a33bd65a8bbd87108913150e
AhmedAbdallah142 <ahmed.abdalla_142@yahoo.com>
2022-03-13 16:02:36 +0200

    Push back end

--- src/test/java/App/ApplicationTests.java
+++ src/test/java/App/ApplicationTests.java
@@ -1,14 +1,20 @@
 package App;
 
 import App.controller.*;
-import App.services.interfaces.DonationService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ApplicationTests {
 	@Autowired
 	AuthenticationController authenticationController;
@@ -31,6 +37,10 @@ class ApplicationTests {
 	@Autowired
 	UserController userController;
 
+	@Autowired
+	private MockMvc mockMvc;
+
+
 	@Test
 	void contextLoads() {
 		assertThat(authenticationController).isNotNull();
@@ -42,6 +52,11 @@ class ApplicationTests {
 		assertThat(userController).isNotNull();
 	}
 
+	@Test
+	public void checkAuthority() throws Exception {
+		this.mockMvc.perform(get("/")).andExpect(status().is(401));
+		this.mockMvc.perform(get("/auth/login")).andExpect(status().is(405));
+	}
 
 
 }
